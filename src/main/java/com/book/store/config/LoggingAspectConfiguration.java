d08c37dbf05f85d0d24ce12c0040b91e257dc6c9
abduganijavaphile <abduganixudoyqulov01@gmail.com>
2023-04-11 20:19:17 +0500

    Initial version of bookStore generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/book/store/config/LoggingAspectConfiguration.java
@@ -0,0 +1,17 @@
+package com.book.store.config;
+
+import com.book.store.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.*;
+import org.springframework.core.env.Environment;
+import tech.jhipster.config.JHipsterConstants;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    @Profile(JHipsterConstants.SPRING_PROFILE_DEVELOPMENT)
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect(env);
+    }
+}
